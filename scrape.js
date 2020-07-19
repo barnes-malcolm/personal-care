efe2c84559d0084191c6eb046454ca61c651e80c
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-07-19 02:12:27 -0500

    added scrape file with initial code for scrapping project data needed

---
+++ scrape.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.csv')
+
+request('URL', (err, res, html) => {
+    if (!err && res.statusCode == 200) {
+        const $ = cheerio.load(html);
+        //jquery here
+
+
+        //Write to csv
+        writeStream.write(`${}, ${} \n`)
+    });
+
+console.log('Scraping Done...')
+}
+});
