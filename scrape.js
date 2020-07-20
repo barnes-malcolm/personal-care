bbe2db749e4a19836b03667bfce55e01598f31d3
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-07-19 19:36:25 -0500

    installed dependencies

--- scrape.js
+++ scrape.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const writeStream = fs.createWriteStream('post.csv')
