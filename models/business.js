d85e5a141eab099637a81c870336f113e3c60a60
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-07-26 18:37:59 -0500

    moved files around, updated routes, and models

---
+++ models/business.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+
+const sequelize = require('../config/connection');
+
+module.exports = function (sequelize, DataTypes) {
+    const Business = sequelize.define("Business", {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        name: DataTypes.STRING,
+        description: DataTypes.TEXT,
+        imageUrl: DataTypes.STRING,
+        phone: DataTypes.STRING,
+        address: DataTypes.STRING,
+        website: DataTypes.STRING,
+        category: {
+            type: DataTypes.STRING,
+            defaultValue: 'Beauty Salons'
+        }
+
+    });
+    return Business;
+};
--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const exphbs = require('express-handlebars');
 const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
-// const passport = require("./config/passport");
+const passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 const db = require("./models");
 const sequelize = require('./config/connection')
 
@@ -26,19 +26,19 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 // We need to use sessions to keep track of our user's login status
-// app.use(
-//   session({
-//     secret: "keyboard cat",
-//     resave: true,
-//     saveUninitialized: true
-//   })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: true,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Requiring our routes
-// require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
