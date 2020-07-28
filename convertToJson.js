1df8c208522e097212f3ee19185ff6b5415f5b51
Ehson Raza <ehson.raza1@outlook.com>
2020-07-27 22:34:14 -0500

    Json Obj and Seeds data

--- convertToJson.js
+++ convertToJson.js
@@ -1,22 +1,72 @@
-const CSVToJSON = require('csvtojson');
-const fs = require('fs');
+const CSVToJSON = require("csvtojson");
+const csvFilePath = "./data/beauty_tx.csv";
+const fs = require("fs");
 
 // convert users.csv file to JSON array
-CSVToJSON().fromFile('./businesses.csv')
-    .then(businesses => {
-
-        // users is a JSON array
-        // log the JSON array
-        console.log(businesses);
-    }).catch(err => {
-        // log error if any
-        console.log(err);
-
+CSVToJSON()
+  .fromFile("./beauty_tx.csv")
+  .then((jsonObj) => {
+    // console.log(jsonObj);
+    for (var key in jsonObj[0]) {
+      console.log(key, jsonObj[0][key]);
+    }
+    var newData = [];
+    jsonObj.map((item) => {
+      var newPlace = {
+        name: item["all_list"],
+        description: item["business description"],
+        phone: item["phone number"],
+        address: item.business_address,
+        website: item["website-href"],
+        image: item["image-Url-href"],
+      };
+      var isUnique = true;
+      newData.filter((item) => {
+        //console.log(item);
+        var count = 0;
+        for (var key in item) {
+          if (item[key] == newPlace[key]) {
+            count++;
+          }
+        }
+        if (count >= 6) {
+          isUnique = false;
+        }
+      });
+      if (isUnique) {
+        newData.push(newPlace);
+      }
     });
+    console.log(newData);
+    fs.writeFile(
+      "./seed_data.json",
+      JSON.stringify(newData, null, 200),
+      (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log("JSON array is saved.");
+      }
+    );
+  })
+  .catch((err) => {
+    // log error if any
+    console.log(err);
+  });
 
-// fs.writeFile('beauty_tx.json', JSON.stringify(businesses, null, 200), (err) => {
-//         if (err) {
-//             throw err;
-//         }
-//         console.log("JSON array is saved.");
+// const csv = require("csvtojson");
+// //TODO Finish Casting from CSV
+// csv()
+//   .fromFile(csvFilePath)
+//   .then((jsonObj) => {
+//     console.log(jsonObj);
+//     const newData = jsonObj.map((item) => {
+//       return {
+//         name: item.name,
+//         description: item.description,
+//         phone: item["phone number"],
+//         address,
+//       };
 //     });
+//     //TODObulkCreate
+//   });
