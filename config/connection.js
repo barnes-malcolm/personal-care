f2d717e60fac7c9ff7c21e48783afe1f3e918499
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-07-29 12:28:23 -0500

    deleted one of the config files

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("personal_caredb", "root", "", {
-  host: "127.0.0.1",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000,
-  },
-});
+// const sequelize = new Sequelize("personal_caredb", "root", "", {
+//   host: "127.0.0.1",
+//   port: 3306,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000,
+//   },
+// });
 
-// Exports the connection for other files to use
-module.exports = sequelize;
+// // Exports the connection for other files to use
+// module.exports = sequelize;
